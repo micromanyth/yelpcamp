8745833fb0365ecc85cb446b1da150e24a6553f4
micromanyth <miromanyth@gmail.com>
2017-08-11 17:24:42 -0400

    db hooked up

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost");
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    assert      = require('assert'),
+    mongoose    = require('mongoose')
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
-  {name: "Granite Hill", image: "https://farm9.staticflickr.com/8225/8524305204_43934a319d.jpg"},
-  {name: "Mountain Goat's Rest", image: "https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg"}
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
 
-
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 // ROUTES
@@ -24,24 +24,31 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, campgrounds){
+    if(err){
+      console.log(err);
+    } else {
+       res.render("campgrounds", {campgrounds: campgrounds});
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image}
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
 });
-
-
-
-
 
 
 // LISTENERS
