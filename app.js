afea950e5c46283eac2a4d80be09249dbc28c3ae
micromanyth <miromanyth@gmail.com>
2017-08-22 07:08:15 -0400

    refactor: split routes up, now using express router

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var express     = require('express'),
     User        = require('./models/user'),
     seedDB      = require('./seeds');
 
+var commentRoutes    = require('./routes/comments'),
+    campgroundRoutes = require('./routes/campgrounds'),
+    indexRoutes      = require('./routes/index');
+
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -29,99 +33,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// ROUTES
-app.get("/", function(req, res) {
-  res.render("landing");
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
 });
 
-app.get("/campgrounds", function(req, res) {
-  Campground.find({}, function(err, allCampgrounds){
-    if(err){
-      console.log(err);
-    } else {
-       res.render("campgrounds/index", {campgrounds: allCampgrounds});
-    }
-  });
-});
-
-app.post("/campgrounds", function(req, res) {
-  var name = req.body.name;
-  var image = req.body.image;
-  var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc}
-  Campground.create(newCampground, function(err, newlyCreated){
-    if(err){
-      console.log(err);
-    } else {
-      res.redirect("/campgrounds");
-    }
-  });
-});
-
-app.get("/campgrounds/new", function(req, res) {
-  res.render("campgrounds/new");
-});
-
-app.get("/campgrounds/:id", function(req, res) {
-  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("campgrounds/show", {campground: foundCampground});
-    }
-  });
-});
-
-// Comments ROUTES
-app.get("/campgrounds/:id/comments/new", function(req, res){
-  Campground.findById(req.params.id, function(err, campground){
-    if (err){
-      console.log(err);
-    } else {
-      res.render("comments/new", {campground: campground});
-    }
-  });
-});
-
-app.post("/campgrounds/:id/comments", function(req, res) {
-  Campground.findById(req.params.id, function(err, campground){
-    if(err){
-      console.log(err);
-      res.redirect("/campgrounds");
-    } else {
-      Comment.create(req.body.comment, function(err, comment){
-        if(err){
-          console.log(error);
-        } else {
-          campground.comments.push(comment);
-          campground.save();
-          res.redirect("/campgrounds/" + campground._id);
-        }
-      });
-    }
-  });
-});
-
-// ===========
-// AUTH Routes
-// ===========
-app.get("/register", function(req, res){
-  res.render("register");
-});
-
-app.post("/register", function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
-      console.log(err);
-      return res.render("register");
-    }
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/campgrounds");
-    });
-  });
-});
-
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments/", commentRoutes);
 
 // LISTENERS
 var url = 'mongodb://localhost:27017/';
